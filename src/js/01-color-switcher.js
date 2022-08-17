afad7947443c7998da6f0bf469c4d1ff124a5574
MykolaH1 <mh22051993@gmail.com>
2022-08-17 17:03:23 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const bodyEl = document.querySelector('body');
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+
+let timerId = null;
+
+
+buttonStart.addEventListener("click", () => {
+   timerId = setInterval(() => {
+      bodyEl.style.backgroundColor = getRandomHexColor();
+      buttonStart.setAttribute("disabled", "disabled");
+   }, 1000);
+ });
+
+
+ buttonStop.addEventListener("click", () => {
+   clearInterval(timerId);
+   buttonStart.removeAttribute("disabled");
+ });
